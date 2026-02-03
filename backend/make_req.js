36175120a17d63a3944b3f04ff79d8073e6f29ea
sgreenvoss <sgreenvoss@gmail.com>
2026-02-03 13:54:30 -0800

    call auth again

--- backend/make_req.js
+++ backend/make_req.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 //const {testConnection} = require('./data_interface.js');
 const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/person');
+//const userRoutes = require('./routes/person');
 //const { attachUser } = require('./middleware/auth');
 
 app.use(cors()); // this is just for development - do origin, credentials when deployed
@@ -40,7 +40,27 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/auth', authRoutes);
-app.use('api/users', userRoutes);
+// app.use('api/users', userRoutes);
+
+app.get('/', async (req, res) => {
+//     // Generate a secure random state value.
+  const state = crypto.randomBytes(32).toString('hex');
+  // Store state in the session
+  req.session.state = state;
+
+  // Generate a url that asks permissions for the Drive activity and Google Calendar scope
+  const authorizationUrl = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+    /** Pass in the scopes array defined above.
+      * Alternatively, if only one scope is needed, you can pass a scope URL as a string */
+    scope: scopes,
+    // Enable incremental authorization. Recommended as a best practice.
+    include_granted_scopes: true,
+    // Include the state parameter to reduce the risk of CSRF attacks.
+    state: state
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
