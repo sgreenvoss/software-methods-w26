7e7fbd4902507d9546cb16d819a00538b1777fa8
sgreenvoss <sgreenvoss@gmail.com>
2026-02-03 21:52:28 -0800

    forgot about oauthclient

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,12 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+
 const scopes = [
   'https://www.googleapis.com/auth/calendar.readonly',
   'https://www.googleapis.com/auth/userinfo.email',
