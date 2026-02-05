9737b331cd4374e13ed3f41f1b3a71979dbb08ac
sgreenvoss <sgreenvoss@gmail.com>
2026-02-05 08:15:32 -0800

    debug checks

--- frontend/js/api/api.js
+++ frontend/js/api/api.js
@@ -1,4 +1,4 @@
-
+// temp fix: added the backend url to the apiget function
 
 
 export async function apiGet(path) {
@@ -9,7 +9,7 @@ export async function apiGet(path) {
 }
 
 export async function apiPost(path, data) {
-  const response = await fetch(path, {
+  const response = await fetch("https://scheduler-backend-9b2i.onrender.com" + path, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",
