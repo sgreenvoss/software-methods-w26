2c818a10c2e16344444220d0f7ada5a46cd19439
sgreenvoss <sgreenvoss@gmail.com>
2026-02-21 11:51:47 -0800

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:8080';
 
 export async function apiGet(path) {
   const response = await fetch(BACKEND_URL + path, {
