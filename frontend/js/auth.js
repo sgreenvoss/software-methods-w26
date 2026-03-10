202c40afb18f09f0b49b5edc7c17cf254929c925
Garrett <gmcaldwell0205@gmail.com>
2026-03-09 21:44:00 -0700

    file cleanup: deleted frontend/css; deleted frontend/js; deleted frontend/old-stuff

--- frontend/js/auth.js
+++
@@ -1,5 +0,0 @@
-import { apiGet } from '../src/api.js'
-
-export async function getCurrentUser() {
-    return apiGet("/api/me");
-}
--- frontend/js/login.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("loginBtn").onclick = () => {
-  var username = document.getElementById('username').value;
-  console.log("username is: " + username);
-  if (!username || username.length > 12) {
-    console.log('try another username please :)');
-  } else {
-    window.location.href = `/auth/google?username=${encodeURIComponent(username)}`;
-  }
-};
